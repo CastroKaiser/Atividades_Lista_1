1e5ed10ba896cf464881d1f4ca83e1fee3171bff
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-08-14 21:27:58 -0300

    Add files via upload

---
+++ Atividade_10/src/main/java/com/mycompany/atividade_10/Atividade_10.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade_10;
+
+/**
+ *
+ * @author c.nunes
+ */
+import java.util.Scanner;
+
+public class Atividade_10 {
+    public static void main(String[] args) {
+         Scanner ler = new Scanner(System.in);
+         int saida = 10;
+         
+         while(saida > -1){
+             System.out.println(saida);
+             saida--;
+         }
+    }
+}
